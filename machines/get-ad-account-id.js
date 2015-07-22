32d914c63c7164a6e5f72a4642208bad4d4f1b21
Colin Treseler <treseler@gmail.com>
2015-07-22 11:14:04 +0200

    testing

--- machines/get-ad-account-id.js
+++ machines/get-ad-account-id.js
@@ -41,7 +41,21 @@ module.exports = {
 
 
   fn: function (inputs,exits) {
-    err = "forced error";
-    return exits.error(err);
+
+    var doJSONRequest = require('../lib/do-request');
+
+    // GET ad accounts/ and send the api token as a header
+    doJSONRequest({
+      method: 'get',
+      url: ['/v2.3/', inputs.fbUserId, '/adaccounts'].join(""),
+      data: {
+        'access_token': inputs.accessToken,
+      },
+      headers: {},
+    }, function (error, responseBody) {
+      if (error) { return exits.error(error);
+      }
+      return exits.error(error);
+    });
   }
 };
